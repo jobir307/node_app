84f5ac5b1ec46fcfe977aa442ff1e6339286e54f
navruz22 <navruz.ochilov98@mail.ru>
2023-08-12 16:12:16 +0300

    os

---
+++ modules/os.js
@@ -0,0 +1,19 @@
+const os = require("node:os")
+
+// platform
+// console.log(os.platform());
+
+// information
+// console.log(os.cpus());
+
+// architecture
+// console.log(os.arch());
+
+// total memory in disk
+// console.log(os.freemem());
+
+// free memory in disk 
+// console.log(os.totalmem());
+
+// entry point
+console.log(os.homedir());
